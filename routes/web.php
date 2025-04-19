0885a76bbacfe042af9f60695d63b3b062eb08f9
kharebovOleg <xarebov1993@yandex.ru>
2025-04-19 22:38:01 +0300

    lesson 10

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\ArticleController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('about', function () {
-    return view('about');
-})->name('about');
+Route::get('about', [PageController::class, 'about'])
+    ->name('about');
+
+Route::get('articles', [ArticleController::class, 'index'])
+    ->name('articles.index');
