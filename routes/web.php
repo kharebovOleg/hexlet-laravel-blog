4a83427981c0096cd5875a24c0faf4e5d437241c
kharebovOleg <xarebov1993@yandex.ru>
2025-04-20 14:00:07 +0300

    lesson 11

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,6 @@ Route::get('about', [PageController::class, 'about'])
 
 Route::get('articles', [ArticleController::class, 'index'])
     ->name('articles.index');
+
+Route::get('articles/{id}', [ArticleController::class, 'show'])
+    ->name('articles.show');
